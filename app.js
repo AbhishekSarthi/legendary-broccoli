a68e8c449bdb4af9796ae7d6d9a2e80e1ec3c850
Abhishek Sarthi <abhi.sarthi15@gmail.com>
2021-04-25 22:19:39 +0530

    second commit

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ let plus = document.querySelector('#plus');
 let minus = document.querySelector('#minus');
 let quantity = document.querySelector('#quantity');
 let payable = document.querySelector('#payable');
+let mini1 = document.querySelector('#mini1');
+let mini2 = document.querySelector('#mini2');
 
 let value = 1;
 let finalAmt = 299;
@@ -21,6 +23,26 @@ function finalAmount() {
     amount.innerHTML = finalAmt * quantity.innerHTML;
 }
 
+function setMini1() {
+    mini1.classList.add('green');
+    mini2.classList.remove('green');
+    finalAmt = 299;
+    finalAmount();
+    // amount.innerHTML = finalAmt;
+    console.log(mini2, mini1);
+}
+
+function setMini2() {
+    mini2.classList.add('green');
+    mini1.classList.remove('green');
+    finalAmt = 599;
+    finalAmount();
+    // amount.innerHTML = finalAmt;
+    console.log(mini2, mini1);
+}
+
 plus.addEventListener('click', () => increment());
 minus.addEventListener('click', () => decrement());
 payable.addEventListener('click', () => finalAmount());
+mini1.addEventListener('click', () => setMini1());
+mini2.addEventListener('click', () => setMini2());
